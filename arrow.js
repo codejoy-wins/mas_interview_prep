d1b781a3389527a80e7f9651b9371e19270ac761
Max <mjann@ucsc.edu>
2019-02-03 13:04:06 -0800

    es6

---
+++ arrow.js
@@ -0,0 +1,13 @@
+const customFunc = ()=> {
+    console.log("custom");
+}
+
+customFunc();
+
+const newFunc = ()=> 18
+
+console.log(newFunc());
+
+const multiply = (a,b)=> a*b
+
+console.log(multiply(3,7));
